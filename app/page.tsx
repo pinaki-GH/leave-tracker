cb7c6e07535906c808309d8c5a4593ec1381067c
pinaki-GH <pinaki4frndz@gmail.com>
2026-01-06 13:20:51 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import LeaveForm from "@/components/LeaveForm";
+import LeaveList from "@/components/LeaveList";
+
+export default function Home() {
+  return (
+    <>
+      <LeaveForm />
+      <LeaveList />
+    </>
+  );
+}
